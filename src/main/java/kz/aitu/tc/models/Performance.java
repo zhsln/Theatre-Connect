bf05a03d272b21d29a86b62ec91cc0ebc0859de2
Nurassyl <153646406+nurikesh@users.noreply.github.com>
2024-03-01 23:58:55 +0600

    Add files via upload

--- src/main/java/kz/aitu/tc/models/Performance.java
+++ src/main/java/kz/aitu/tc/models/Performance.java
@@ -1,6 +1,7 @@
 package kz.aitu.tc.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -13,10 +14,20 @@ public class Performance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id; // Unique identifier for each performance.
+
+    @NotBlank(message = "Title of the performance is mandatory.")
     private String title;
+
+    @NotBlank(message = "Date of the performance is mandatory.")
     private LocalDate date; // yyyy-MM-dd
+
+    @NotBlank(message = "Time of the performance is mandatory.")
     private OffsetTime time; // HH:mm:ss + HH:mm (time with timezone)
+
+    @NotBlank(message = "Duration of the performance is mandatory.")
     private int duration; // In minutes.
+
+    @NotBlank(message = "Venue of the performance is mandatory.")
     private String venue; /*
                             Venue means place of the event.
                             For example, "The State Opera and Ballet Theatre 'Astana Opera'".
