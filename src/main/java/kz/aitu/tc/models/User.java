1ef4642a28adf207f2b0a7274777e558faa38792
Nurassyl <153646406+nurikesh@users.noreply.github.com>
2024-03-01 23:58:14 +0600

    Add files via upload

--- src/main/java/kz/aitu/tc/models/User.java
+++ src/main/java/kz/aitu/tc/models/User.java
@@ -1,6 +1,9 @@
 package kz.aitu.tc.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -10,10 +13,24 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id; // Unique identifier for each user.
+
+    @NotBlank(message = "Login is mandatory.") // for validation.
+    @Size(min = 5, message = "Login must be at least 5 characters long.") // size matters!
     private String login;
+
+    @NotBlank(message = "Password is mandatory.")
+    @Size(min = 8, message = "Password must be at least 8 characters long.")
     private String password;
+
+    @NotBlank(message = "Name is mandatory.")
     private String name;
+
+    @NotBlank(message = "Surname is mandatory.")
     private String surname;
-    private boolean editor; // Editor is administrator that can upload and change performances in Database and Website.
-    private boolean manager; // Manager is administrator that can upload and change everything.
+
+    @NotNull(message = "Please specify whether the user is an editor.")
+    private Boolean editor; // Editor is administrator that can upload and change performances in Database and Website.
+
+    @NotNull(message = "Please specify whether the user is a manager.")
+    private Boolean manager; // Manager is administrator that can upload and change everything.
 }
